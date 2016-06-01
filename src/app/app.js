7a6544143eb9e8d12366fe7ff26b896d6faf5441
lazantsy <christophe.lazantsy@gmail.com>
2016-06-01 13:00:35 +0200

    add ng-material suite

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,40 @@
 import angular from 'angular';
-
+import 'angular-material';
 import '../style/app.css';
 
 let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
-};
+    return {
+        template: require('./app.html'),
+        controller: 'AppCtrl',
+        controllerAs: 'app'
+    }
+}
 
 class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
+    constructor(NameService) {
+        console.log('appCtrl');
+        NameService.getName().then((data)=>{
+             console.log('data : ',data);
+        })
+    }
+};
+
+class NameService {
+    constructor($q) {
+        this._$q= $q;
+    };
+
+    getName() {
+        return this._$q.when('christophe lazantsy');
+    }
 }
+
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+angular.module(MODULE_NAME, ['ngMaterial'])
+    .directive('app', app)
+    .controller('AppCtrl',AppCtrl)
+    .service('NameService',NameService);
 
 export default MODULE_NAME;
