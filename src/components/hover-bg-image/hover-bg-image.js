f7f461c392aab3446c1c1e62954575f711f5b7b7
lazantsy <christophe.lazantsy@gmail.com>
2016-06-10 11:20:14 +0200

    add rollover on sidepanel

--- src/components/hover-bg-image/hover-bg-image.js
+++ src/components/hover-bg-image/hover-bg-image.js
@@ -14,53 +14,25 @@ class HoverBgImageCtrl {
 export default class HoverBgImage {
     constructor() {
         this.restrict = 'EA';
-        this.scope = {};
-
         this.controller = HoverBgImageCtrl;
         this.controllerAs = 'ctrl';
-        this.bindToController = true;
     }
 
     compile(elm){
-        console.log('elm: ',elm)
-        elm.css('backgroundImage','url(assets/img/vision.png)')
-        elm.css('backgroundRepeat','no-repeat')
-        elm.css('backgroundAttachment','inherit')
-        elm.css('backgroundPosition','right center')
         return this.link.bind(this);
     }
 
     link(scope, elm, attrs){
         elm.bind('mouseenter', ()=> {
 
-            elm.css('backgroundImage','url(assets/img/vision-rollover.png)')
-            elm.css('backgroundRepeat','no-repeat')
-            elm.css('backgroundAttachment','inherit')
-            elm.css('backgroundPosition','right center')
-            elm.css('color','#ffeecc)')
+            elm.css('backgroundImage','url('+attrs.hoverBgImage+')')
             elm.css('borderRight','think solid #ffeecc)')
         });
         elm.bind('mouseleave', ()=> {
-            elm.css('backgroundImage','url(assets/img/vision.png)')
-            elm.css('backgroundRepeat','no-repeat')
-            elm.css('backgroundAttachment','inherit')
-            elm.css('backgroundPosition','right center')
+            elm.css('backgroundImage','url('+attrs.bgImage+')')
         })
 
     }
-    /*
-     return {
-     link: ((scope, elm, attrs)=> {
-     elm.bind('mouseenter', ()=> {
-     this.style.backgroundImage = 'url(' + attrs.hoverBgImage + ')';
-     });
-     elm.bind('mouseleave', ()=> {
-     this.style.backgroundImage = '';
-     })
-
-     })
-     }
-     */
 }
 
 angular.module(MODULE_NAME, [])
